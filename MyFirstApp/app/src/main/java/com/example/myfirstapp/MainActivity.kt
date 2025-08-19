39a49e8ac3c1f3786d2d8e695722146d6af7a553
suganrajan34 <suganrajan34@gmail.com>
2025-08-19 14:31:13 +0530

    initial commit

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyFirstAppTheme {
-        Greeting("Android sugan")
+        Greeting("Android sugan hi local")
     }
 }
