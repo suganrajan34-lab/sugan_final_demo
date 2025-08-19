99b324ac83e9d590f39ff80c1518c0c3781ce593
suganrajan34 <suganrajan34@gmail.com>
2025-08-19 16:42:12 +0530

    sugan new demo commit

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android new")
+        Greeting("Android new commit ")
     }
 }
