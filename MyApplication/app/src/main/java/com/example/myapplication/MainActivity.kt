de0de1a01e6563b2e0a314e263bae4d306fd15cb
suganrajan34-lab <suganrajan34@gmail.com>
2025-08-19 16:43:10 +0530

    pull code

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android new commit push ")
+        Greeting("Android new commit push pull ")
     }
 }
